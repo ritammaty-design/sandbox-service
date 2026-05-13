a36aebeaafbf655bde2ae499dcb4fc436610bc1c
ritammaty-design <ritammaty@gmail.com>
2026-05-13 05:58:48 +0000

    feat(kubernetes): add Kubernetes client and initial pod configuration
    - Added @kubernetes/client-node dependency to package.json.
    - Created config.js to load Kubernetes configuration and initialize CoreV1Api client.
    - Created pod.js to import the k8sCoreV1Api for future pod management functionalities.

---
+++ sandbox/server/src/kubernetes/config.js
@@ -0,0 +1,7 @@
+import *as k8s from "@kubernetes/client-node";
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+
+export const k8sCoreV1Api = kc.makeApiClient(k8s.CoreV1Api);
---
+++ sandbox/server/src/kubernetes/pod.js
@@ -0,0 +1,4 @@
+import { k8sCoreV1Api } from "./config.js";
+
+
+
