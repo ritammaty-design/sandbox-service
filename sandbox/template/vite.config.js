e7526df7576c55a3d3777ca12b6d5a06b1034cf5
ritam-g <ritammaty2006@gmail.com>
2026-05-15 18:04:40 +0530

    fix(dockerfile): update Dockerfile to build and serve production assets

--- sandbox/template/vite.config.js
+++ sandbox/template/vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  base: './',
-
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-    hmr: false,
-    allowedHosts: 'all'
-  }
+  base: './'
 })
