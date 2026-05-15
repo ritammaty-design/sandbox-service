2675f9e391f66110080e6904e95cdf5793b7c9c2
ritam-g <ritammaty2006@gmail.com>
2026-05-15 16:49:46 +0530

    style(vite): format server configuration for improved readability

--- sandbox/template/vite.config.js
+++ sandbox/template/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  
+  server: {
     host: '0.0.0.0',
     port: 5173,
+
+    strictPort: true,
+
+    hmr: false,
+
     allowedHosts: true
   }
 })
