c613da891b729423bf5d13679ebc371fa60bcbce
ritam-g <ritammaty2006@gmail.com>
2026-05-18 22:45:07 +0530

    feat: implement file reading endpoint and add Dockerfile for agent service

--- sandbox/agent/src/app.js
+++ sandbox/agent/src/app.js
@@ -1,8 +1,9 @@
 import morgan from 'morgan'
 import express from 'express'
-
+import fs from 'node:fs/promises'
 const app = express()
-
+// for the template docker build file 
+const WROKDIR = '/workspace'
 
 app.use(morgan('dev'))
 
@@ -11,5 +12,18 @@ app.get('/', (req, res) => {
     return res.status(200).json({ status: 'ok' })
 });
 
-
+app.get('/read-files', async function (req, res) {
+    try {
+        const Files = await fs.readdir(WROKDIR)
+        return res.status(200).json({
+            message: 'files reads successfully',
+            files: Files
+        })
+    } catch (error) {
+        console.log('====================================');
+        console.log(error);
+        console.log('====================================');
+        return res.status(500).json({ error: error.message })
+    }
+})
 export default app
